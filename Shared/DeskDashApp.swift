790b36d05b67a6ee4dff5e173504932758b217f8
Thiago Bittencourt Coelho <thiago.bittc@gmail.com>
2022-04-12 22:07:11 -0300

    Initial Commit

---
+++ Shared/DeskDashApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeskDashApp.swift
+//  Shared
+//
+//  Created by PremierSoft on 12/04/22.
+//
+
+import SwiftUI
+
+@main
+struct DeskDashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
